15ccd18c582ca9aca04f736df4b750c87b669a1b
tuanlm1201 <83169461+tuanlm1201@users.noreply.github.com>
2021-11-09 16:03:33 +0700

    update slide review

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,25 @@
+// SLIDE review 
+
 $(document).ready(function () {
-  $(".customer-list").slick({
+  $(".review-list").slick({
     accessibility: true,
     autoplay: true,
     speed: 300,
     dots: true,
     draggable: true,
+    arrows: true,
+    nextArrow: `<button type="button" class="slick-next"><i class="icofont-arrow-right"></i></button>`,
+    prevArrow: `<button type="button" class="slick-prev"><i class="icofont-arrow-left"></i></button>`,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+          dots: false,
+          draggable: false,
+        }
+      }
+    ]
   });
 });
 
@@ -16,18 +31,18 @@ const headerMenuClose = document.querySelector('.header-menu__close');
 const menuItem = document.querySelector('.header-menu__link')
 
 toggleMenu.addEventListener('click', handleToggleMenu);
-function handleToggleMenu(){
+function handleToggleMenu() {
   menuBG.classList.add('is-active');
   headerMenu.classList.add('is-active');
 }
 
 headerMenuClose.addEventListener('click', handleCloseMenu);
-function handleCloseMenu(){
+function handleCloseMenu() {
   menuBG.classList.remove('is-active');
   headerMenu.classList.remove('is-active');
 }
 
-menuItem.addEventListener('click', function(){
+menuItem.addEventListener('click', function () {
   console.log('menuItem')
   menuBG.classList.remove('is-active');
   headerMenu.classList.remove('is-active');
