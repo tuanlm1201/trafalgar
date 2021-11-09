3a7ff32e8d16d3803f9bec6a31bdda473a1297f2
tuanlm1201 <83169461+tuanlm1201@users.noreply.github.com>
2021-11-09 14:44:09 +0700

    responesive

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,28 @@ $(document).ready(function () {
     draggable: true,
   });
 });
+
+// Toggle-menu
+const toggleMenu = document.querySelector('.header-toggle');
+const menuBG = document.querySelector('.header-menu__bg')
+const headerMenu = document.querySelector('.header-menu');
+const headerMenuClose = document.querySelector('.header-menu__close');
+const menuItem = document.querySelector('.header-menu__link')
+
+toggleMenu.addEventListener('click', handleToggleMenu);
+function handleToggleMenu(){
+  menuBG.classList.add('is-active');
+  headerMenu.classList.add('is-active');
+}
+
+headerMenuClose.addEventListener('click', handleCloseMenu);
+function handleCloseMenu(){
+  menuBG.classList.remove('is-active');
+  headerMenu.classList.remove('is-active');
+}
+
+menuItem.addEventListener('click', function(){
+  console.log('menuItem')
+  menuBG.classList.remove('is-active');
+  headerMenu.classList.remove('is-active');
+});
